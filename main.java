fd9cc0ae96e9c83e4ab385f8eebe341cc08afcb7
HingYipWu <hingyipwu@gmail.com>
2024-03-16 23:02:58 -0400

    gykty

--- main.java
+++ main.java
@@ -1,10 +1,14 @@
- import TheWYipWuCode.wCodeRightHere;
+import Carlos.carlosMain;
+import HingYip.hingMain;
  public class main{
-    private static wCodeRightHere x;
+    
+   private static hingMain HingYip;
+   private static carlosMain Carlos;
  public static void main(String[] args)
     {
         System.out.println("hi");
-        x = new wCodeRightHere();
+        HingYip = new hingMain();
+        Carlos = new carlosMain();
 
     }
  }
